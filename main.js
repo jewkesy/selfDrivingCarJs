387f9f3e74459e13b895b359e97585b18b301724
Daryl Jewkes <git@daryljewkes.com>
2023-02-14 20:45:23 +0000

    end of tutorial

--- main.js
+++ main.js
@@ -10,10 +10,24 @@ const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width/2, carCanvas.width*0.9);
 
 const cars = generateCars(100);
-let bestCar;
+let bestCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+	for (let i = 0; i < cars.length; i++) {
+		cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+		if (i != 0) {
+			NeuralNetwork.mutate(cars[i].brain, 0.1);
+		}
+	}
+}
 
 const traffic = [
-	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
+	new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 1.75),
+	new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2.5),
+	new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+	new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2.5),
+	new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2.75),
+	new Car(road.getLaneCenter(3), -700, 30, 50, "DUMMY", 2.25),
 ];
 
 animate();
